708f667b6f477c5d51cba499350ebb4941d9ca8e
emirbesir <emirbesirx@gmail.com>
2025-03-05 20:17:44 +0300

    chore(challenge): challenge assets are added

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     [SerializeField] private float topBound = 30;
-    [SerializeField] private float lowerBound = -10;
+    [SerializeField] private float lowerBound = -5;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,6 +20,7 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,23 +2,25 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [Header("Spawn Manager Prefab Settings")]
     [SerializeField] private GameObject[] animalPrefabs;
+    [Header("Spawn Manager Position Settings")]
     [SerializeField] private float spawnRangeX = 15f;
     [SerializeField] private float spawnPosZ = 20f;
+    [Header("Spawn Manager Timing Settings")]
+    [SerializeField] private float startDelay = 2f;
+    [SerializeField] private float spawnInterval = 1.5f;
+    private string spawnMethodName = "SpawnRandomAnimal";
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        InvokeRepeating(spawnMethodName, startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            SpawnRandomAnimal();
-        }
     }
 
     private void SpawnRandomAnimal()
