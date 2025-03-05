296a9c0a829fa77131cc1946017b45eeb201e3c4
emirbesir <emirbesirx@gmail.com>
2025-03-05 19:58:21 +0300

    feat(spawn): enemy spawn manager added

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] animalPrefabs;
+    [SerializeField] private float spawnRangeX = 15f;
+    [SerializeField] private float spawnPosZ = 20f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SpawnRandomAnimal();
+        }
+    }
+
+    private void SpawnRandomAnimal()
+    {
+        int spawnIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[spawnIndex], spawnPos, animalPrefabs[spawnIndex].transform.rotation);
+    }
+}
