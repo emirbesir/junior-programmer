6642959fefa5dae623513f2c011e81156ec83508
emirbesir <emirbesirx@gmail.com>
2025-03-05 18:53:45 +0300

    feat(player): keepin the player inbounds logic added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
     private float horizontalInput;
+    private int xRange = 15;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -16,6 +17,14 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         transform.Translate(horizontalInput * Time.deltaTime * speed * Vector3.right);
     }
 }
