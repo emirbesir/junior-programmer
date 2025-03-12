dad79ac7a4fd63a5499b17352c842fc02727f4af
emirbesir <emirbesirx@gmail.com>
2025-03-12 03:55:58 +0300

    feat(player): add ground detection for jump mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,18 +2,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float gravityModifier = 1f;
+    [SerializeField] private bool isOnGround = true;
     private Rigidbody playerRb;
+
     private void Awake()
     {
         playerRb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
+    private void Start()
+    {
+        Physics.gravity *= gravityModifier;
+    }
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
