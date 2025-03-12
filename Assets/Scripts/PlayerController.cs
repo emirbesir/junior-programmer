2cdda2721c544e5d48b1f3175b60f5dd12631f89
emirbesir <emirbesirx@gmail.com>
2025-03-12 20:05:25 +0300

    refactor(player): playerController organized

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float movementSpeed = 10.0f;
-    private float xBounds = 25f;
-    private float zBounds = 25f;
+    [Header("Bounds")]
+    [SerializeField] private float xBounds = 25f;
+    [SerializeField] private float zBounds = 25f;
+
     private float horizontalInput;
     private float verticalInput;
     private Rigidbody playerRb;
@@ -17,15 +20,29 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        GetInput();
     }
 
     private void FixedUpdate()
     {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+    void GetInput()
+    {
+        // Get the player's input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+    void MovePlayer()
+    {
+        // Move the player based on the input
         Vector3 movement = movementSpeed * new Vector3(horizontalInput, 0, verticalInput);
         playerRb.linearVelocity = movement;
+    }
 
+    void ConstrainPlayerPosition()
+    {
         // Clamp the player's position within the bounds
         playerRb.position = new Vector3(
             Mathf.Clamp(playerRb.position.x, -xBounds, xBounds),
