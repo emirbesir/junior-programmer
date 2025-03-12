d467ef5b437151d6b1ae55761605a08ce7a2a854
emirbesir <emirbesirx@gmail.com>
2025-03-12 20:52:59 +0300

    feat(player): implement basic player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5.0f;
+    private GameObject focalPoint;
+    private Rigidbody playerRb;
+    private void Awake()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
