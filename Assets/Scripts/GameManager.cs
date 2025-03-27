023b10011d5eed18f6db546731db68e0315534cf
emirbesir <emirbesirx@gmail.com>
2025-03-27 18:04:56 +0300

    feat(game manager): implement target spawning system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,23 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> targets;
+    [SerializeField] private float spawnRate = 1.0f;
+    private void Start()
+    {
+        StartCoroutine(SpawnTarget());
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
 }
