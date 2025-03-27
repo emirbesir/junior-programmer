5e35516479269cd9c8ecf40a9fc8a7ed268d41fa
emirbesir <emirbesirx@gmail.com>
2025-03-27 18:02:38 +0300

    refactor(target): cleanup the code

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,13 +2,33 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
     private Rigidbody rb;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
-        rb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
-        transform.position = new Vector3(Random.Range(-4, 4), -6);
+        rb.AddForce(RandomForce(), ForceMode.Impulse);
+        rb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
     }
 }
