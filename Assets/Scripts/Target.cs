bd5586d489da03b8c7e9c2c3325adac72ecb459c
emirbesir <emirbesirx@gmail.com>
2025-03-27 17:56:56 +0300

    feat(target): add physics-based movement and randomization

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,15 +2,13 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private Rigidbody rb;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        rb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+        transform.position = new Vector3(Random.Range(-4, 4), -6);
     }
 }
