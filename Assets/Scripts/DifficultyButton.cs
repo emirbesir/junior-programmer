558c051fc33e96e27c040910fbee9fea2ed2c014
emirbesir <emirbesirx@gmail.com>
2025-03-31 18:04:20 +0300

    feat(gameplay): implement difficulty selection and title screen logic

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -3,16 +3,19 @@ using UnityEngine.UI;
 
 public class DifficultyButton : MonoBehaviour
 {
+    [SerializeField] private int difficulty;
     private Button button;
+    private GameManager gameManager;
 
     private void Start()
     {
         button = GetComponent<Button>();
         button.onClick.AddListener(SetDifficulty);
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     private void SetDifficulty()
     {
-        Debug.Log(gameObject.name + " was clicked");
+        gameManager.StartGame(difficulty);
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,21 +11,16 @@ public class GameManager : MonoBehaviour
     [SerializeField] private float spawnRate = 1.0f;
     public bool isGameActive = true;
 
-    [Header("UI Settings")]
+    [Header("Score Settings")]
     [SerializeField] private int score;
     [SerializeField] private TextMeshProUGUI scoreText;
+
+    [Header("Title Screen Settings")]
+    [SerializeField] private GameObject titleScreen;
 
     [Header("Game Over Settings")]
     [SerializeField] private TextMeshProUGUI gameOverText;
     [SerializeField] private GameObject restartButton;
-
-    private void Start()
-    {
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(score);
-        isGameActive = true;
-    }
 
     IEnumerator SpawnTarget()
     {
@@ -49,9 +44,19 @@ public class GameManager : MonoBehaviour
         restartButton.SetActive(true);
         isGameActive = false;
     }
-
     public void RestartGame() 
     { 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        isGameActive = true;
+        score = 0;
+        StartCoroutine(SpawnTarget());
+        UpdateScore(score);
+        titleScreen.SetActive(false);
+        scoreText.gameObject.SetActive(true);
     }
 }
