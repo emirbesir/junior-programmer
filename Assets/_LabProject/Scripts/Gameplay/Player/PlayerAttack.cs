5c7e201aa5fc77daa8a7c67eb0f54f004aca952a
Emir <emirbesirx@gmail.com>
2025-06-13 20:27:09 +0300

    feat: change mouse button input from down to hold for player attack

--- Assets/_LabProject/Scripts/Gameplay/Player/PlayerAttack.cs
+++ Assets/_LabProject/Scripts/Gameplay/Player/PlayerAttack.cs
@@ -15,7 +15,7 @@ public class PlayerAttack : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) && Time.time >= _nextAttackTime)
+        if (Input.GetMouseButton(0) && Time.time >= _nextAttackTime)
         {
             PerformAttack();
             _nextAttackTime = Time.time + _config.TimeBetweenAttacks;
